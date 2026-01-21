2d9e55dfb9f81c1e1630b8be6f96ec7e710ae592
kk-zhao <kk-zhao@example.com>
2026-01-21 18:45:35 +0800

    优化一下seo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'henryDai';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Chenghan Dai';
+export const SITE_DESCRIPTION = 'The personal blog and portfolio of Chenghan Dai. Sharing thoughts on technology, design, and life.';
