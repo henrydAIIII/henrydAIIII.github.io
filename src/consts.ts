3c3d5c8be51d97e507a0b0352b31e1a6a6d516cc
kk-zhao <kk-zhao@example.com>
2026-01-21 11:22:30 +0800

    垃圾提交

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '小戴今天写什么';
-export const SITE_DESCRIPTION = 'I told you, sooner or later I will become a millionaire';
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
